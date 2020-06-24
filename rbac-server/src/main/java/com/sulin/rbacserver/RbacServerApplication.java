21e5895a4ff024c7548e8f4338856c9c2c0a522d
sulin <sulinwork@163.com>
2020-06-24 08:32:01 +0800

    --init init project

---
+++ rbac-server/src/main/java/com/sulin/rbacserver/RbacServerApplication.java
@@ -0,0 +1,13 @@
+package com.sulin.rbacserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RbacServerApplication.class, args);
+    }
+
+}
---
+++ rbac-server/src/test/java/com/sulin/rbacserver/RbacServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sulin.rbacserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
